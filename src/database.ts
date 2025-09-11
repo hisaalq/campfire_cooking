befb6043839301e461b9841f54e1fcdd103d86ed
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-09-11 19:18:22 +0300

    fix database connection

--- src/database.ts
+++ src/database.ts
@@ -5,7 +5,7 @@ dotenv.config();
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("DB_URL");
+    const conn = await mongoose.connect(process.env.DB_URL!);
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error("MongoDB connection failed:", error);
