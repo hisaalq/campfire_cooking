23a104727bc0254c144b71523ba0a13052c5ec46
Sayuri Cero <Hisaalqaoud@gmail.com>
2025-09-14 18:54:01 +0300

    middleware fixed

--- src/middleware/verifyUser.ts
+++ src/middleware/verifyUser.ts
@@ -2,22 +2,21 @@ import { Request, Response, NextFunction } from 'express';
 import jwt from 'jsonwebtoken';
 
 export function authorize(req: Request, res: Response, next: NextFunction) {
-    const header = req.headers.authorization;
-    console.log(header)
-    if (!header) {
-        res.status(401).json({ message: 'No token provided' });
-    }
+  const header = req.headers.authorization;
+  if (!header) {
+    return res.status(401).json({ message: 'No token provided' });
+  }
 
-    const [scheme, token] = header?.split(' ') || [];
-    if (scheme !== 'Bearer' || !token) {
-        res.status(401).json({ message: 'Invalid auth format' });
-    }
+  const [scheme, token] = header.split(' ');
+  if (scheme !== 'Bearer' || !token) {
+    return res.status(401).json({ message: 'Invalid auth format' });
+  }
 
-    try {
-        const payload = jwt.verify(token, process.env.JWT_SECRET!);
-        (req as any).user = payload;
-        next();
-    } catch (err) {
-        res.status(401).json({ message: 'Invalid or expired token' });
-    }
+  try {
+    const payload = jwt.verify(token, process.env.JWT_SECRET!);
+    (req as any).user = payload;
+    return next();
+  } catch (err) {
+    return res.status(401).json({ message: 'Invalid or expired token' });
+  }
 }
